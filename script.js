d56c8f99cec5c179cb7c9dde0920a893b3ebc84b
fuksikbrik <93256876+fuksikbrik@users.noreply.github.com>
2023-09-09 13:02:36 +0300

    .

---
+++ script.js
@@ -0,0 +1,52 @@
+const sliderItem = document.querySelectorAll('.slider-item')
+
+const textFromSlide = document.querySelectorAll('.slider-item-text')
+
+let func = function (){
+
+
+    textFromSlide.forEach(item => {
+        console.log(item.offsetHeight);
+       if(item.offsetHeight >= 80){
+       
+        let ahtung = document.createElement('button')
+        ahtung.innerHTML = `<img src="./dots.svg" alt="">`;
+        item.appendChild(ahtung)
+        ahtung.classList.add('btns')
+        ahtung.addEventListener('click', function () {
+              item.parentNode.classList.toggle('active-slide')
+        })
+       }
+    })
+
+
+
+
+
+
+    // sliderItem.forEach(item => {
+    //     if(item.offsetHeight >= 200){
+    //     let ahtung = document.createElement('button')
+    //     ahtung.innerHTML = `Открыть`;
+    //     item.appendChild(ahtung)
+    //     ahtung.classList.add('btns')
+    //     }
+    // });
+};
+func();
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 2.5,
+    spaceBetween: 40,
+    
+   
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+
+
+
+
